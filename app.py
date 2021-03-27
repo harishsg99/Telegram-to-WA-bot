e3df478f90fd9d9afdde49e3d5dd9891d42bca18
Harish S.G <harishsg99@gmail.com>
2021-03-27 19:48:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,72 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import telebot
 import requests
 import re
 import os
 from twilio.rest import Client
+import pyrebase
+bot = telebot.TeleBot("1733329720:AAHlKaQ5ONTA85WFhvOvpdn9Wi11qfBZ2Ms", parse_mode=None)
+config = {
+  "apiKey": "AIzaSyDPmfNoXoIO9S7TRH1eYtgqNY-Ny3kak9E",
+  "authDomain": "wbtotelebot.firebaseapp.com",
+  "databaseURL": "https://wbtotelebot-default-rtdb.firebaseio.com",
+  "storageBucket": "wbtotelebot.appspot.com"
+}
 
-def bop(bot, update):
-    url = get_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
-def main():
-    updater = Updater('YOUR_TOKEN')
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
-    updater.start_polling()
-    updater.idle()
-
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    a = message.text
+    b = a.split('@')
+    chatt = message[-1].chat.id
+    if(b[0] == "/start")
+        bot.reply_to(message, "Welcome to tele2WA bot")
+    elif(b[0] == "/setsid")
+        data = {
+              "sid": b[1]
+             }
+  
+        db.child("users").child(chatt).set(data)
+        bot.reply_to(message, "SID updated")
+    elif(b[0] == "/settoken")
+        data = {
+              "token": b[1]
+             }
+  
+        db.child("users").child(chatt).set(data)
+    elif(b[0] == "/setfromphone")
+        data = {
+              "fromphone": b[1]
+             }
+  
+        db.child("users").child(chatt).set(data)
+    elif(b[0] == "/settophone")
+        data = {
+              "tophone": b[1]
+             }
+        db.child("users").child(chatt).set(data)
+    elif(b[0] == "/updatetophone")
+        data = {
+              "tophone": b[1]
+             }
+        db.child("users").child(chatt).update(data)
+    elif(b[0] == "/updatefromphone")
+        data = {
+              "fromphone": b[1]
+             }
+        db.child("users").child(chatt).update(data)
+    
+    elif(b[0] =="/sendWA")
+        test = db.child("users").child(chatt).get()
+        p = test.val()['sid']
+        q = test.val()['token']
+        t = user.val()['fromphone']
+        r = user.val()['tophone']
+        client = Client(p,q)
+        client.messages.create(body=b[1],from="whatsapp:"+ str(t),to="whatsapp:"+str(r))
+       
 if __name__ == '__main__':
     main()
+
+
